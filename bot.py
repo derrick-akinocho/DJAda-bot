de61eca1bb0815877f552d6c07c5d57d6b61e7b9
Derrick <145935725+derrick-akinocho@users.noreply.github.com>
2025-11-20 10:52:01 +0100

    deleted addwrite

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ class ModmailBot(commands.Bot):
         self.session = None
         self._api = None
         self.formatter = SafeFormatter()
-        self.loaded_cogs = ["cogs.modmail", "cogs.plugins", "cogs.utility", "cogs.ada_write"]
+        self.loaded_cogs = ["cogs.modmail", "cogs.plugins", "cogs.utility"]
         self._connected = None
         self.start_time = discord.utils.utcnow()
         self._started = False
